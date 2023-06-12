f3904584537fb903e077ca05d8c44c010869fe26
Sebasceb <sceballo@espol.edu.ec>
2023-06-11 21:29:23 -0500

    error ortografico

--- src/adapter/Main.java
+++ src/adapter/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	
 	
 	
-	public static void transeferir(Cuenta emisor, double monto, Cuenta receptor) {
+	public static void transferir(Cuenta emisor, double monto, Cuenta receptor) {
 		//asumimos verifacion que no trata de transferir mas de lo que tiene
 		emisor.setCantidad(emisor.getCantidad()-monto);
 		receptor.setCantidad(receptor.getCantidad()+monto);
@@ -20,7 +20,7 @@ public class Main {
 		Cuenta cuenta2= new Cuenta(1000000,Moneda.EURO,"Cuenta2");
 		System.out.println(cuenta2);
 		
-		transeferir(cuenta1, 500, cuenta2);
+		transferir(cuenta1, 500, cuenta2);
 		System.out.println(cuenta1);
 		System.out.println(cuenta2);
 
